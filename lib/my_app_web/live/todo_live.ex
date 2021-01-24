86437a58e27f4811d11330bb3b3d196c851b594a
* <winsalva396@gmail.com>
2021-01-24 08:37:16 +0800

    Made my todo working

--- lib/my_app_web/live/todo_live.ex
+++ lib/my_app_web/live/todo_live.ex
@@ -1,19 +1,17 @@
 defmodule MyAppWeb.TodoLive do
   use MyAppWeb, :live_view
 
-
-
   alias MyApp.Todos
 
 
   def mount(_params, _session, socket) do
+    IO.inspect socket
     {:ok, assign(socket, todos: Todos.list_todos())}
   end
 
 
-  def render(assigns) do
-    ~L"""
-      Rendering liveview 
-    """
-  end
+  def handle_event("add", %{"todo" => todo}, socket) do
+    Todos.create_todo(todo)
+    {:noreply, assign(socket, todos: Todos.list_todos())}
+   end
 end
--- lib/my_app_web/router.ex
+++ lib/my_app_web/router.ex
@@ -20,6 +20,7 @@ defmodule MyAppWeb.Router do
     live "/", PageLive, :index
     live "/counter", CounterLive
     live "/todos", TodoLive, :index
+    live "/todos/register", TodoLive, :register
   end
 
 
