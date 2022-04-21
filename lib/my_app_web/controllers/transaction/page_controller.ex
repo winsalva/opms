9a0a2815c98f17a5d7c757e7ddb4f948ab952b83
* <winsalva396@gmail.com>
2022-04-21 18:41:41 +0800

    Added new improvements

--- lib/my_app_web/controllers/transaction/page_controller.ex
+++ lib/my_app_web/controllers/transaction/page_controller.ex
@@ -10,32 +10,34 @@ defmodule MyAppWeb.Transaction.PageController do
   def new_transaction(conn, %{"item_id" => id}) do
     negotiation = Negotiation.new_negotiation()  
     item = Item.get_item(id)
+    transaction_params = %{
+      item_id: item.id,
+      seller_company_id: item.company_id,
+      buyer_company_id: conn.assigns.current_user.company_id
+    }
+    {:ok, transaction} = Transaction.insert_transaction(transaction_params)
     params = [
+      transaction_id: transaction.id,
       negotiation: negotiation,
       item: item
     ]
     render(conn, "new-transaction.html", params)
   end
 
-  def create_transaction(conn, %{"negotiation" => %{"msg" => msg}, "item_id" => item_id}) do
+  def create_transaction(conn, %{"negotiation" => %{"msg" => msg}, "item_id" => item_id, "transaction_id" => transaction_id}) do
     item = Item.get_item(item_id)
-    transaction_params = %{
-      item_id: item.id,
-      seller_company_id: item.company_id,
-      buyer_company_id: conn.assigns.current_user.company_id
-    }
-    case Transaction.insert_transaction(transaction_params) do
-      {:ok, transaction} ->
-        case Negotiation.insert_negotiation(%{msg: msg, transaction_id: transaction.id, user_id_id: conn.assigns.current_user.id}) do
-	  {:ok, _negotiation} ->
-	    conn
-	    |> redirect(to: Routes.user_page_path(conn, :show, conn.assigns.current_user.id))
-	    _ -> conn
-      	    |> redirect(to: Routes.user_page_path(conn, :show, conn.assigns.current_user.id))
-	end
-     _ ->
-       conn
-       |> redirect(to: Routes.user_page_path(conn, :show, conn.assigns.current_user.id))
+    case Negotiation.insert_negotiation(%{msg: msg, transaction_id: transaction_id, user_id: conn.assigns.current_user.id}) do
+      {:ok, _negotiation} ->
+        conn
+	|> redirect(to: Routes.user_page_path(conn, :show, conn.assigns.current_user.id))
+      {:error, %Ecto.Changeset{} = negotiation} ->
+        params = [
+	  item: Item.get_item(item_id),
+	  negotiation: negotiation,
+	  transaction_id: transaction_id
+	]
+        conn
+	|> render("new-transaction.html", params)
     end
   end
 end
