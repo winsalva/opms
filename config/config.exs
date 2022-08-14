cd711a623800911bec3d767324921ff7bfc4766f
winsalva <winsalva396@gmail.com>
2022-08-14 14:56:56 +0800

    Added updates

--- config/config.exs
+++ config/config.exs
@@ -5,7 +5,7 @@
 # is restricted to this project.
 
 # General application configuration
-use Mix.Config
+import Config
 
 config :my_app,
   ecto_repos: [MyApp.Repo]
--- config/dev.exs
+++ config/dev.exs
@@ -1,4 +1,4 @@
-use Mix.Config
+import Config
 
 # Configure your database
 config :my_app, MyApp.Repo,
