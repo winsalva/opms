be93fca7cf464c78725e5ccbf31809774bf43a0b
winsalva <winsalva396@gmail.com>
2022-12-13 21:03:47 +0800

    Add fix

--- priv/repo/migrations/20221213073744_alter_companies_add_seen.exs
+++ priv/repo/migrations/20221213073744_alter_companies_add_seen.exs
@@ -5,5 +5,7 @@ defmodule MyApp.Repo.Migrations.AlterCompaniesAddSeen do
     alter table(:companies) do
       add :seen, :boolean, default: false
     end
+
+    create unique_index(:companies, [:department])
   end
 end
